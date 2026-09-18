5142deb8fb3b001afff9abe08caa00d5ac1594a7
Geeta Ram <geeta.ram@cloudsmartz.net>
2026-09-18 14:09:07 +0530

    ui fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import Script from 'next/script';
 import { GOOGLE_ADS_ID } from '@/lib/googleAds';
+import { META_PIXEL_ID } from '@/lib/metaPixel';
 import './globals.css';
 import { Poppins } from 'next/font/google';
 import { MotionConfig } from 'framer-motion';
@@ -80,6 +81,27 @@ gtag('js', new Date());
 gtag('config', '${GOOGLE_ADS_ID}');`}
         </Script>
         <Script id="google-ads-base" src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_ID}`} strategy="afterInteractive" />
+        <Script id="meta-pixel-init" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${META_PIXEL_ID}');
+fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <img
+            height={1}
+            width={1}
+            alt=""
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+          />
+        </noscript>
         <NavigationTracker />
         {/* `reducedMotion="user"` makes every framer-motion `motion.*` component sitewide
             automatically honor the OS-level `prefers-reduced-motion` setting (disabling
---
+++ lib/metaPixel.ts
@@ -0,0 +1,1 @@
+export const META_PIXEL_ID = '1089461426999958';
